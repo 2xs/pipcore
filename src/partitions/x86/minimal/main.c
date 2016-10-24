3dcc24da877d9a6600e19f6800a21f7415768770
Mahieddine Yaker <mahieddine.yaker@etudiant.univ-lille1.fr>
2016-10-24 15:14:58 +0200

    Add some example code using the LibPip

--- src/partitions/x86/minimal/main.c
+++ src/partitions/x86/minimal/main.c
@@ -1,9 +1,12 @@
 #include <stdint.h>
 #include <pip/fpinfo.h>
 #include <pip/debug.h>
-
+#include <pip/api.h>
+#include <pip/vidt.h>
 void main(pip_fpinfo* bootinfo)
 {
-    puts("Hello world!\n");
+    vsti();
+    notify(0,66);
+    //puts("Hello world!\n");
     for(;;);
-}  
+}
