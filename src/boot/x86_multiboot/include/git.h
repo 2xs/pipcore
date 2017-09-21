872d12e27777a535590779ffb662169ff2c9551d
David Nowak <david.nowak@lifl.fr>
2017-09-21 15:38:39 +0200

    Remove generated file

--- src/boot/x86_multiboot/include/git.h
+++
@@ -1,8 +0,0 @@
-#ifndef __GIT__
-#define __GIT__
-<<<<<<< HEAD
-#define GIT_REVISION "d06df222b16f6ffb2124d942dc00872a5c3dfde6"
-=======
-#define GIT_REVISION "ecae4dc8bc08c35428eb0689797754766a5204f1"
->>>>>>> origin/develop
-#endif
