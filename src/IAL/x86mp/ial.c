1ad06d5acb96a00515ec4436edc9240cc94b7d7b
Quentin Bergougnoux <xedac@MacBook-Pro-de-Quentin.local>
2017-11-12 12:11:21 +0100

    Comment out APIC timer output - no longer required

--- src/IAL/x86mp/ial.c
+++ src/IAL/x86mp/ial.c
@@ -389,7 +389,7 @@ genericHandler (int_ctx_t *is)
     if(is->int_no == 32) {
         timer_ticks++;
         //IAL_DEBUG(CRITICAL, "Interrupt %d occured through APIC, ticks: %d\n", is->int_no, timer_ticks);	
-        kprintf(".");
+        //kprintf(".");
     }
     /* Send EOI signal to APIC */
     write_lapic(APIC_EOI, 0x0);   
